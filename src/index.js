bfeb5e92637b7c77c859d884a2005799430c95be
Petyo Ivanov <underlog@gmail.com>
2019-05-28 16:08:09 +0300

    Add Material UI

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { Virtuoso } from 'react-virtuoso'
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {Virtuoso} from 'react-virtuoso';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const ListContainer = ({listRef, style, children}) => {
+  return (
+    <List ref={listRef} style={style}>
+      {children}
+    </List>
+  );
+};
+
+const ItemContainer = ({children, ...props}) => {
+  return (
+    <ListItem {...props} style={{margin: 0}}>
+      {children}
+    </ListItem>
+  );
+};
 
 const App = () => {
-return  <Virtuoso
-      style={{width: '200px', height: '400px'}}
+  return (
+    <Virtuoso
+      ListContainer={ListContainer}
+      ItemContainer={ItemContainer}
+      style={{width: '400px', height: '400px'}}
       totalCount={500}
-      item={index => <div>Item {index}</div>}
+      item={index => <ListItemText primary={`Item ${index}`} />}
     />
-
-}
+  );
+};
 
 render(<App />, document.getElementById('root'));
